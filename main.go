658c0629e1b95f0bd24eb8e601111d4a63b98759
Maxwell Mbugua <maksywriter@gmail.com>
2022-01-24 11:52:33 +0300

    Done with the Start Of Frame Marker (0xFFC0)
    The next marker to handle will be the Define Huffman Table Marker

--- main.go
+++ main.go
@@ -25,6 +25,135 @@ func (bf *Buffer) advance() {
 	bf.bf[0] = data[0]
 }
 
+func decodeAPPN(header *Header) {
+	fmt.Printf("** Decoding APPN Marker (0xFF%X) **\n", header.buffer.bf[0])
+	buf := header.buffer
+	buf.advance()
+	buf.advance()
+	// Length includes the 2 bytes that give you the length
+	length := (int(buf.bf[1]) << 8) + int(buf.bf[0]) - 2
+	for a := 0; a < length; a++ {
+		buf.advance()
+	}
+}
+
+func decodeQuantizationTables(header *Header) {
+	buf := header.buffer
+	fmt.Printf("** Decoding the DQT Marker (0xFF%X) **\n", buf.bf[0])
+	buf.advance()
+	buf.advance()
+	length := (int(buf.bf[1]) << 8) + int(buf.bf[0]) - 2
+	for {
+		if length <= 0 {
+			break
+		}
+		buf.advance()
+		tableId := int(buf.bf[0] & 0x0F)
+		length -= 1
+		if tableId > 3 {
+			fmt.Printf("Error! Ivalid TableId: %d\n", tableId)
+			os.Exit(1)
+		}
+		// If the upper nibble is non-zero then the table is 16bit
+		bit16 := (buf.bf[0] >> 4) != 0
+		table := [64]byte{}
+		if bit16 {
+			for a := 0; a < 64; a++ {
+				buf.advance()
+				buf.advance()
+				table[zigzag[a]] = (buf.bf[1] << 8) + buf.bf[0]
+			}
+			length -= 128
+		} else {
+			for a := 0; a < 64; a++ {
+				buf.advance()
+				table[zigzag[a]] = buf.bf[0]
+			}
+			length -= 64
+		}
+		// Check if a table with the same ID already exist
+		for t := range header.qTables {
+			if tableId == header.qTables[t].Id {
+				fmt.Printf("Error! More than one Quantization Table with the same ID: %d\n", tableId)
+				os.Exit(1)
+			}
+		}
+		header.qTables = append(header.qTables, QuantizationTable{Id: tableId, table: table})
+	}
+	fmt.Printf("\n")
+}
+
+func decodeStartOfFrame(h *Header) {
+	fmt.Printf("** Decoding Start Of Frame (0xFF%X) **\n", h.buffer.bf[0])
+	buf := h.buffer
+	buf.advance()
+	buf.advance()
+	length := (int(buf.bf[1]) << 8) + int(buf.bf[0]) - 2
+	buf.advance()
+	length -= 1
+	if buf.bf[0] != 8 {
+		fmt.Printf("Error! Invalid Precision (%d). Expected 8\n", buf.bf[0])
+		os.Exit(1)
+	}
+	buf.advance()
+	buf.advance()
+	length -= 2
+	width := (int(buf.bf[1]) << 8) + int(buf.bf[0])
+	buf.advance()
+	buf.advance()
+	length -= 2
+	height := (int(buf.bf[1]) << 8) + int(buf.bf[0])
+	buf.advance()
+	length -= 1
+	components := int(buf.bf[0])
+	if components > 3 {
+		fmt.Printf("Error! Number of components > 3. CMYK ColorMode not supported\n")
+		os.Exit(1)
+	}
+	zeroBased := false
+	for a := 0; a < components; a++ {
+		buf.advance()
+		length -= 1
+		// TODO: Add Error handling for when compId > 3 and of YIQ color mode (id=4,5)
+		compId := int(buf.bf[0])
+		buf.advance()
+		length -= 1
+		hSamplingFactor := int(buf.bf[0]) >> 4
+		vSamplingFactor := int(buf.bf[0]) & 0x0F
+		buf.advance()
+		length -= 1
+		qTableId := int(buf.bf[0])
+		// Check if the Id is zero based
+		if compId == 0 {
+			zeroBased = true
+		}
+		// Check if the component alredy exist, Duplicate Ids
+		for c := range h.cComponents {
+			comp := &h.cComponents[c]
+			if compId == comp.Id {
+				fmt.Printf("Error! Duplicate coponentId (%d) found when scanning 'START OF FRAME'\n", compId)
+				os.Exit(1)
+			}
+		}
+
+		if zeroBased {
+			compId += 1
+		}
+		h.cComponents = append(h.cComponents, ColorComponent{
+			Id:              compId,
+			vSamplingFactor: vSamplingFactor,
+			hSamplingFactor: hSamplingFactor,
+			qTableId:        qTableId,
+		})
+	}
+	h.width = width
+	h.height = height
+	h.print()
+	if length != 0 {
+		fmt.Printf("Error! Invalid Start Of Frame\n")
+	}
+}
+
 func decodeJPEG(filename string) {
 	file, err := os.Open(filename)
 	if err != nil {
@@ -33,18 +162,138 @@ func decodeJPEG(filename string) {
 	}
 	stat, _ := file.Stat()
 	wd, _ := os.Getwd()
-	fmt.Printf("Filename: [%s]\n", path.Join(wd, file.Name()))
-	fmt.Printf("Filesize: [%d] Bytes\n\n", stat.Size())
+	_filename := path.Join(wd, file.Name())
+	_filesize := stat.Size()
 	buffer := Buffer{f: file}
+	// Create the header
+	header := &Header{
+		filename: _filename,
+		filesize: uint(_filesize),
+		buffer:   &buffer,
+	}
 	buffer.advance()
 	buffer.advance()
-	if buffer.bf[1] != 0xFF && buffer.bf[0] != 0xD8 {
+	if buffer.bf[1] != 0xFF && buffer.bf[0] != SOI {
 		fmt.Printf("Error! The file is not a valid JPEG\n")
 		os.Exit(1)
 	}
-	fmt.Printf("The file is a valid JPEG\n")
+	// For loop for parsig all the markers
+	for {
+		buffer.advance()
+		buffer.advance()
+		if buffer.bf[1] != 0xFF {
+			fmt.Printf("Error! Expected a Marker but found byte (%x)\n", buffer.bf[1])
+			os.Exit(1)
+		}
+		if buffer.bf[0] >= APP0 && buffer.bf[0] <= APP15 {
+			decodeAPPN(header)
+		}
+		if buffer.bf[0] == DQT {
+			decodeQuantizationTables(header)
+		}
+		if buffer.bf[0] == SOF0 {
+			decodeStartOfFrame(header)
+		}
+	}
 	fmt.Printf("******************************\n\n")
 	file.Close()
+}
+
+func pad(bt byte) string {
+	val := fmt.Sprintf("%d", int(bt))
+	rem := 3 - len(val)
+	for a := 0; a < rem; a++ {
+		val = " " + val
+	}
+	return val
+}
+
+var zigzag = [64]byte{
+	0, 1, 8, 16, 9, 2, 3, 10,
+	17, 24, 32, 25, 18, 11, 4, 5,
+	12, 19, 26, 33, 40, 48, 41, 34,
+	27, 20, 13, 6, 7, 14, 21, 28,
+	35, 42, 49, 56, 57, 50, 43, 36,
+	29, 22, 15, 23, 30, 37, 44, 51,
+	58, 59, 52, 45, 38, 31, 39, 46,
+	53, 60, 61, 54, 47, 55, 62, 63,
+}
+
+func (header *Header) print() {
+	fmt.Printf("Filename : [%s]\n", header.filename)
+	fmt.Printf("Filesize : [%d] Bytes\n", header.filesize)
+	fmt.Printf("\n")
+	fmt.Printf("*** Quantization Tables ***\n")
+	for t := range header.qTables {
+		table := header.qTables[t]
+		fmt.Printf("Id (%d)\n", table.Id)
+		for entry := range table.table {
+			if entry%8 == 0 {
+				fmt.Printf("\n")
+			}
+			fmt.Printf("%s", pad(table.table[entry]))
+		}
+		fmt.Printf("\n\n")
+	}
+	fmt.Printf("*** Start Of Frame ***\n")
+	fmt.Printf("Size       : %d x %d\n", header.width, header.height)
+	fmt.Printf("Components : %d\n", len(header.cComponents))
+	for a := range header.cComponents {
+		comp := header.cComponents[a]
+		fmt.Printf("** ComponentId (%d) **\n", comp.Id)
+		fmt.Printf("Horizontal Sampling Factor : %d\n", comp.hSamplingFactor)
+		fmt.Printf("Vertical Sampling Factor   : %d\n", comp.vSamplingFactor)
+		fmt.Printf("Quantization Table Id      : %d\n", comp.qTableId)
+	}
+	fmt.Printf("\n")
+}
+
+// Markers
+const (
+	SOI = 0xD8
+	/** APPN **/
+	APP0  = 0xE0
+	APP1  = 0xE1
+	APP2  = 0xE2
+	APP3  = 0xE3
+	APP4  = 0xE4
+	APP5  = 0xE5
+	APP6  = 0xE6
+	APP7  = 0xE7
+	APP8  = 0xE8
+	APP9  = 0xE9
+	APP10 = 0xEA
+	APP11 = 0xEB
+	APP12 = 0xEC
+	APP13 = 0xED
+	APP14 = 0xEE
+	APP15 = 0xEF
+	/**/
+	DQT = 0xDB
+	/** Start Of Frame **/
+	SOF0 = 0xC0
+)
+
+type QuantizationTable struct {
+	table [64]byte
+	Id    int
+}
+
+type Header struct {
+	filename    string
+	filesize    uint
+	buffer      *Buffer
+	qTables     []QuantizationTable
+	cComponents []ColorComponent
+	width       int
+	height      int
+}
+
+type ColorComponent struct {
+	Id              int
+	hSamplingFactor int
+	vSamplingFactor int
+	qTableId        int
 }
 
 func main() {
