863e3da6550fce7f16b94e4e9dd6e54398553527
Maxwell Mbugua <maksywriter@gmail.com>
2022-01-26 14:26:51 +0300

    Completed huffman decoding
    Also I have handled all the markers that might appear in the jpeg file

--- main.go
+++ main.go
@@ -80,7 +80,6 @@ func decodeQuantizationTables(header *Header) {
 		}
 		header.qTables = append(header.qTables, QuantizationTable{Id: tableId, table: table})
 	}
-	fmt.Printf("\n")
 }
 
 func decodeStartOfFrame(h *Header) {
@@ -110,7 +109,6 @@ func decodeStartOfFrame(h *Header) {
 		fmt.Printf("Error! Number of components > 3. CMYK ColorMode not supported\n")
 		os.Exit(1)
 	}
-	zeroBased := false
 	for a := 0; a < components; a++ {
 		buf.advance()
 		length -= 1
@@ -125,7 +123,7 @@ func decodeStartOfFrame(h *Header) {
 		qTableId := int(buf.bf[0])
 		// Check if the Id is zero based
 		if compId == 0 {
-			zeroBased = true
+			h.zeroBased = true
 		}
 		// Check if the component alredy exist, Duplicate Ids
 		for c := range h.cComponents {
@@ -145,15 +143,153 @@ func decodeStartOfFrame(h *Header) {
 	}
 	h.width = width
 	h.height = height
-	if zeroBased {
+	if h.zeroBased {
 		for a := range h.cComponents {
 			h.cComponents[a].Id += 1
 		}
 	}
-	h.print()
 	if length != 0 {
 		fmt.Printf("Error! Invalid Start Of Frame\n")
 	}
+}
+
+func decodeRestartInterval(header *Header) {
+	buf := header.buffer
+	fmt.Printf("** Decoding Define Restart Interval (0xFF%X)**\n", buf.bf[0])
+	buf.advance()
+	buf.advance()
+	length := (int(buf.bf[1]) << 8) + int(buf.bf[0]) - 2
+	if length != 2 {
+		fmt.Printf("Error! Invalid Restart Interval Length (%d)\n", length)
+		os.Exit(1)
+	}
+	buf.advance()
+	buf.advance()
+	restartInterval := (int(buf.bf[1]) << 8) + int(buf.bf[0])
+	header.restartInterval = restartInterval
+}
+
+func decodeDefineHuffmanTable(header *Header) {
+	buf := header.buffer
+	fmt.Printf("** Decoding Define Huffman Table (0xFF%X) **\n", buf.bf[0])
+	buf.advance()
+	buf.advance()
+	length := (int(buf.bf[1]) << 8) + int(buf.bf[0]) - 2
+	for {
+		if length <= 0 {
+			break
+		}
+		buf.advance()
+		length -= 1
+		dc := (buf.bf[0] >> 4) == 0
+		tableId := int(buf.bf[0] & 0x0F)
+		// Create the new table
+		table := HuffmanTable{
+			Id: tableId,
+			dc: dc,
+		}
+		// Read the codes of len
+		count := 0
+		for a := 0; a < 16; a++ {
+			buf.advance()
+			length -= 1
+			table.codesOfLen[a] = int(buf.bf[0])
+			count += int(buf.bf[0])
+		}
+		for a := 0; a < count; a++ {
+			buf.advance()
+			length -= 1
+			table.symbols = append(table.symbols, buf.bf[0])
+		}
+		header.huffmanTables = append(header.huffmanTables, table)
+	}
+	if length != 0 {
+		fmt.Printf("Error! Invalid DefineHuffanTable Marker\n")
+	}
+}
+
+func endScan(header *Header) {
+	buf := header.buffer
+	fmt.Printf("*** EOI (0xFF%X) ***\n", buf.bf[0])
+	header.print()
+}
+
+func decodeStartOfScan(header *Header) {
+	buf := header.buffer
+	fmt.Printf("** Decoding Start of Scan (0xFF%X) **\n", buf.bf[0])
+	buf.advance()
+	buf.advance()
+	length := (int(buf.bf[1]) << 8) + int(buf.bf[0]) - 2
+	buf.advance()
+	length -= 1
+	components := int(buf.bf[0])
+	for a := 0; a < components; a++ {
+		buf.advance()
+		length -= 1
+		compId := int(buf.bf[0])
+		if header.zeroBased {
+			compId += 1
+		}
+		buf.advance()
+		length -= 1
+		dcHuffmanTableId := buf.bf[0] >> 4
+		acHuffmanTableId := buf.bf[0] & 0x0F
+		// Assign the AC and DC Huffman Table Ids to the components
+		for c := range header.cComponents {
+			comp := &header.cComponents[c]
+			if compId == comp.Id {
+				comp.acHuffmanTableId = int(acHuffmanTableId)
+				comp.dcHuffmanTableId = int(dcHuffmanTableId)
+			}
+		}
+	}
+	buf.advance()
+	length -= 1
+	header.startOfSelection = buf.bf[0]
+	buf.advance()
+	length -= 1
+	header.endOfSelection = buf.bf[0]
+	buf.advance()
+	length -= 1
+	header.successiveApproximationHigh = buf.bf[0] >> 4
+	header.successiveApproximationLow = buf.bf[0] & 0x0F
+	/** Begin the SCAN **/
+	buf.advance()
+	for {
+		// The only markers that are allowed in the scan are RSTn Markers and the EOI Marker
+		if buf.bf[0] == 0xFF {
+			buf.advance()
+			if buf.bf[0] == 0xFF {
+				buf.advance()
+				continue
+			} else if buf.bf[0] >= RST0 && buf.bf[0] <= RST7 {
+				buf.advance()
+				continue
+			} else if buf.bf[0] == EOI {
+				endScan(header)
+				break
+			} else if buf.bf[0] == 0x00 {
+				header.bitstream = append(header.bitstream, 0xFF)
+				buf.advance()
+				continue
+			}
+			fmt.Printf("Ivalid Byte (0xFF%X) found in the bitsteam\n", buf.bf[0])
+			os.Exit(1)
+		}
+		header.bitstream = append(header.bitstream, buf.bf[0])
+		buf.advance()
+	}
+}
+
+func skipMarker(header *Header) {
+	buf := header.buffer
+	buf.advance()
+	buf.advance()
+	length := (int(buf.bf[1]) << 8) + int(buf.bf[0]) - 2
+	for a := 0; a < length; a++ {
+		buf.advance()
+	}
+	fmt.Printf("Skipped Marker (0xFF%X) Len (#%d) Bytes\n", buf.bf[0], length)
 }
 
 func decodeJPEG(filename string) {
@@ -180,24 +316,57 @@ func decodeJPEG(filename string) {
 		os.Exit(1)
 	}
 	// For loop for parsig all the markers
+	buffer.advance()
+	buffer.advance()
 	for {
-		buffer.advance()
-		buffer.advance()
 		if buffer.bf[1] != 0xFF {
 			fmt.Printf("Error! Expected a Marker but found byte (%x)\n", buffer.bf[1])
 			os.Exit(1)
 		}
-		if buffer.bf[0] >= APP0 && buffer.bf[0] <= APP15 {
+		// The standard allows for any number of 0xFF bytes to precede the marker
+		if buffer.bf[0] == 0xFF {
+			buffer.advance()
+			continue
+		} else if buffer.bf[0] >= APP0 && buffer.bf[0] <= APP15 {
 			decodeAPPN(header)
-		}
-		if buffer.bf[0] == DQT {
+		} else if buffer.bf[0] == DQT {
 			decodeQuantizationTables(header)
-		}
-		if buffer.bf[0] == SOF0 {
+		} else if buffer.bf[0] == SOF0 {
 			decodeStartOfFrame(header)
+		} else if buffer.bf[0] == DRI {
+			decodeRestartInterval(header)
+		} else if buffer.bf[0] == DHT {
+			decodeDefineHuffmanTable(header)
+		} else if buffer.bf[0] == SOS {
+			decodeStartOfScan(header)
+			break
+		} else if (buffer.bf[0] >= JPG0 && buffer.bf[0] <= JPG13) ||
+			(buffer.bf[0] == DNL) ||
+			(buffer.bf[0] == DHP) ||
+			(buffer.bf[0] == EXP) ||
+			(buffer.bf[0] == COM) {
+			skipMarker(header)
+		} else if buffer.bf[0] == TEM {
+			// TEM has no size nor payload
+		} else if buffer.bf[0] == EOI {
+			fmt.Printf("Error! Found EOI Marker (0xFF%X) before the Start Of Scan Marker\n", buffer.bf[0])
+			os.Exit(1)
+		} else if buffer.bf[0] == SOI {
+			fmt.Printf("Error! Embeded JPG not supported\n")
+			os.Exit(1)
+		} else if buffer.bf[0] == DAC {
+			fmt.Printf("Error! Arithmetic Coding not supported\n")
+			os.Exit(1)
+		} else if buffer.bf[0] >= SOF0 && buffer.bf[0] <= SOF15 {
+			fmt.Printf("Error! SOF Marker (0xFF%X) not supported\n", buffer.bf[0])
+			os.Exit(1)
+		} else {
+			fmt.Printf("Invalid Marker (0xFF%X)\n", buffer.bf[0])
+			os.Exit(1)
 		}
+		buffer.advance()
+		buffer.advance()
 	}
-	fmt.Printf("******************************\n\n")
 	file.Close()
 }
 
@@ -222,8 +391,14 @@ var zigzag = [64]byte{
 }
 
 func (header *Header) print() {
+	fmt.Printf("\n")
 	fmt.Printf("Filename : [%s]\n", header.filename)
 	fmt.Printf("Filesize : [%d] Bytes\n", header.filesize)
+	fmt.Printf("\n")
+	fmt.Printf("Start Of Selection            : %d\n", header.startOfSelection)
+	fmt.Printf("End of Selection              : %d\n", header.endOfSelection)
+	fmt.Printf("Successive Approximation High : %d\n", header.successiveApproximationHigh)
+	fmt.Printf("Successive Approximation Low  : %d\n", header.successiveApproximationLow)
 	fmt.Printf("\n")
 	fmt.Printf("*** Quantization Tables ***\n")
 	for t := range header.qTables {
@@ -246,14 +421,85 @@ func (header *Header) print() {
 		fmt.Printf("Horizontal Sampling Factor : %d\n", comp.hSamplingFactor)
 		fmt.Printf("Vertical Sampling Factor   : %d\n", comp.vSamplingFactor)
 		fmt.Printf("Quantization Table Id      : %d\n", comp.qTableId)
+		fmt.Printf("AC Huffman Table Id        : %d\n", comp.acHuffmanTableId)
+		fmt.Printf("DC Huffman Table Id        : %d\n", comp.dcHuffmanTableId)
 	}
-	fmt.Printf("\n")
+	fmt.Printf("\n*** Huffman Tables ***\n")
+	for t := range header.huffmanTables {
+		table := header.huffmanTables[t]
+		fmt.Printf("* Id : (%d) ", table.Id)
+		if table.dc {
+			fmt.Printf("DC *\n")
+		} else {
+			fmt.Printf("AC *\n")
+		}
+		lastIndex := 0
+		for a := 0; a < 16; a++ {
+			fmt.Printf("%s  --> ", pad(byte(a+1)))
+			for s := range table.symbols[lastIndex : lastIndex+table.codesOfLen[a]] {
+				fmt.Printf("%x ", table.symbols[s+lastIndex])
+			}
+			lastIndex += table.codesOfLen[a]
+			fmt.Printf("\n")
+		}
+		fmt.Printf("\n")
+	}
+	fmt.Printf("Huffman data length (%d) Bytes\n", len(header.bitstream))
 }
 
 // Markers
 const (
-	SOI = 0xD8
-	/** APPN **/
+	// Start of Frame markers, non-differential, Huffman coding
+	SOF0 = 0xC0 // Baseline DCT
+	SOF1 = 0xC1 // Extended sequential DCT
+	SOF2 = 0xC2 // Progressive DCT
+	SOF3 = 0xC3 // Lossless (sequential)
+
+	// Start of Frame markers, differential, Huffman coding
+	SOF5 = 0xC5 // Differential sequential DCT
+	SOF6 = 0xC6 // Differential progressive DCT
+	SOF7 = 0xC7 // Differential lossless (sequential)
+
+	// Start of Frame markers, non-differential, arithmetic coding
+	SOF9  = 0xC9 // Extended sequential DCT
+	SOF10 = 0xCA // Progressive DCT
+	SOF11 = 0xCB // Lossless (sequential)
+
+	// Start of Frame markers, differential, arithmetic coding
+	SOF13 = 0xCD // Differential sequential DCT
+	SOF14 = 0xCE // Differential progressive DCT
+	SOF15 = 0xCF // Differential lossless (sequential)
+
+	// Define Huffman Table(s)
+	DHT = 0xC4
+
+	// JPEG extensions
+	JPG = 0xC8
+
+	// Define Arithmetic Coding Conditioning(s)
+	DAC = 0xCC
+
+	// Restart interval Markers
+	RST0 = 0xD0
+	RST1 = 0xD1
+	RST2 = 0xD2
+	RST3 = 0xD3
+	RST4 = 0xD4
+	RST5 = 0xD5
+	RST6 = 0xD6
+	RST7 = 0xD7
+
+	// Other Markers
+	SOI = 0xD8 // Start of Image
+	EOI = 0xD9 // End of Image
+	SOS = 0xDA // Start of Scan
+	DQT = 0xDB // Define Quantization Table(s)
+	DNL = 0xDC // Define Number of Lines
+	DRI = 0xDD // Define Restart Interval
+	DHP = 0xDE // Define Hierarchical Progression
+	EXP = 0xDF // Expand Reference Component(s)
+
+	// APPN Markers
 	APP0  = 0xE0
 	APP1  = 0xE1
 	APP2  = 0xE2
@@ -270,10 +516,24 @@ const (
 	APP13 = 0xED
 	APP14 = 0xEE
 	APP15 = 0xEF
-	/**/
-	DQT = 0xDB
-	/** Start Of Frame **/
-	SOF0 = 0xC0
+
+	// Misc Markers
+	JPG0  = 0xF0
+	JPG1  = 0xF1
+	JPG2  = 0xF2
+	JPG3  = 0xF3
+	JPG4  = 0xF4
+	JPG5  = 0xF5
+	JPG6  = 0xF6
+	JPG7  = 0xF7
+	JPG8  = 0xF8
+	JPG9  = 0xF9
+	JPG10 = 0xFA
+	JPG11 = 0xFB
+	JPG12 = 0xFC
+	JPG13 = 0xFD
+	COM   = 0xFE
+	TEM   = 0x01
 )
 
 type QuantizationTable struct {
@@ -281,21 +541,38 @@ type QuantizationTable struct {
 	Id    int
 }
 
+type HuffmanTable struct {
+	Id         int
+	symbols    []byte
+	codesOfLen [16]int
+	dc         bool
+}
+
 type Header struct {
-	filename    string
-	filesize    uint
-	buffer      *Buffer
-	qTables     []QuantizationTable
-	cComponents []ColorComponent
-	width       int
-	height      int
+	filename                    string
+	filesize                    uint
+	buffer                      *Buffer
+	qTables                     []QuantizationTable
+	cComponents                 []ColorComponent
+	width                       int
+	height                      int
+	restartInterval             int
+	huffmanTables               []HuffmanTable
+	startOfSelection            byte
+	endOfSelection              byte
+	successiveApproximationHigh byte
+	successiveApproximationLow  byte
+	bitstream                   []byte
+	zeroBased                   bool
 }
 
 type ColorComponent struct {
-	Id              int
-	hSamplingFactor int
-	vSamplingFactor int
-	qTableId        int
+	Id               int
+	hSamplingFactor  int
+	vSamplingFactor  int
+	qTableId         int
+	acHuffmanTableId int
+	dcHuffmanTableId int
 }
 
 func main() {
